7e157346e949b12edd98531adc0c94fb73e3ceda
Dominik Gogola <d.s.gogola@gmail.com>
2020-05-26 01:14:46 +0200

    - added PhotoVM factory

---
+++ ViewModels/Services/PhotoViewModelFactory.cs
@@ -0,0 +1,36 @@
+﻿using ImageFiltersWPF.Models;
+using ImageFiltersWPF.ViewModels.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Windows.Media.Imaging;
+
+namespace ImageFiltersWPF.ViewModels.Services
+{
+    public class PhotoViewModelFactory : IPhotoViewModelFactory
+    {
+        private readonly ILogger<PhotoViewModelFactory> logger;
+
+        public PhotoViewModelFactory(ILogger<PhotoViewModelFactory> logger)
+        {
+            this.logger = logger;
+        }
+
+        public PhotoViewModel CreatePhotoViewModel(PhotoData data)
+        {
+            logger.LogInformation($"CreatePhotoViewModel() || Path: {data.OriginalPhotoPath}");
+            var photoViewModel = new PhotoViewModel();
+            try
+            {
+                photoViewModel.PhotoData = data;
+                photoViewModel.CurrentImage = new BitmapImage(new Uri(data.CurrentPhotoPath));
+                photoViewModel.OriginalImage = new BitmapImage(new Uri(data.CurrentPhotoPath));
+                return photoViewModel;
+            }
+            catch (Exception)
+            {
+                logger.LogError($"CreatePhotoViewModel() || Path: {data.OriginalPhotoPath}");
+                return null;
+            }
+        }
+    }
+}
